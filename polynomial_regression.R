e6d69ce189a8cdad9da38cedd56539840c838813
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-16 11:38:21 -1000

    R template finished

--- polynomial_regression.R
+++ polynomial_regression.R
@@ -1,14 +1,15 @@
-# Polynomial Regression
+# Data Preprocessing Template
 
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
+
 # Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+# install.packages('caTools')
 # library(caTools)
 # set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
 # training_set = subset(dataset, split == TRUE)
 # test_set = subset(dataset, split == FALSE)
 
@@ -16,19 +17,18 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Linear Regression to the dataset
+#Fitting Linear Regression to the dataset
 lin_reg = lm(formula = Salary ~ .,
              data = dataset)
 
-# Fitting Polynomial Regression to the dataset
+#Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
 
-# Visualising the Linear Regression results
-# install.packages('ggplot2')
+#Visuaslizing the Linear Regression results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
@@ -39,8 +39,7 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Polynomial Regression results
-# install.packages('ggplot2')
+#Visuaslizing the Polynomial Regression results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
@@ -51,28 +50,11 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(poly_reg,
-                                        newdata = data.frame(Level = x_grid,
-                                                             Level2 = x_grid^2,
-                                                             Level3 = x_grid^3,
-                                                             Level4 = x_grid^4))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Polynomial Regression)') +
-  xlab('Level') +
-  ylab('Salary')
+#Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
 
-# Predicting a new result with Linear Regression
-predict(lin_reg, data.frame(Level = 6.5))
-
-# Predicting a new result with Polynomial Regression
-predict(poly_reg, data.frame(Level = 6.5,
-                             Level2 = 6.5^2,
-                             Level3 = 6.5^3,
-                             Level4 = 6.5^4))
+#Predicting a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
--- regression_template.R
+++ regression_template.R
@@ -4,11 +4,12 @@
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
+
 # Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+# install.packages('caTools')
 # library(caTools)
 # set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
 # training_set = subset(dataset, split == TRUE)
 # test_set = subset(dataset, split == FALSE)
 
@@ -16,14 +17,14 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
 
-# Predicting a new result
+#Fitting Regression Model to the dataset
+#Create your regressor here
+
+#Predicting a new result with Polynomial Regression
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
-# install.packages('ggplot2')
+#Visuaslizing the Polynomial Regression results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
@@ -34,15 +35,6 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
+
+
+
