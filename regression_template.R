d1b5fcc65548103c843003679443803fb94f1fa3
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-09-16 11:40:52 -1000

    updated R template

--- regression_template.R
+++ regression_template.R
@@ -21,10 +21,10 @@ dataset = dataset[2:3]
 #Fitting Regression Model to the dataset
 #Create your regressor here
 
-#Predicting a new result with Polynomial Regression
+#Predicting a new
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-#Visuaslizing the Polynomial Regression results
+#Visuaslizing the Regression Model results
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
@@ -34,6 +34,19 @@ ggplot() +
   ggtitle('Truth or Bluff (Regression Model)') +
   xlab('Level') +
   ylab('Salary')
+
+#Visuaslizing the Regression Model results (for higher resolution and smoother curve)
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Regression Model)') +
+  xlab('Level') +
+  ylab('Salary')
+
 
 
 
